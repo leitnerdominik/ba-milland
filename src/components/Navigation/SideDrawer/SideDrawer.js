910883841c9c8cabe51bd59a7d135d74a7d3ac7d
Dominik Leitner <bigtoasti@gmail.com>
2018-11-24 18:45:30 +0100

    added a sidedrawer for mobile devices

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,33 @@
+import React, { Fragment } from 'react';
+
+import Logo from '../../Logo/Logo';
+import Backdrop from '../../Backdrop/Backdrop';
+import NavigationItems from '../NavigationItems/NavigationItems';
+
+import classes from './SideDrawer.module.css';
+
+const sideDrawer = ({open, closed}) => {
+
+  let attachedClasses = [classes.SideDrawer, classes.Close];
+
+  if (open) {
+    attachedClasses = [classes.SideDrawer, classes.Open];
+  }
+
+  return (
+    <Fragment>
+      <Backdrop show={open} clicked={closed} />
+      <div className={attachedClasses.join(' ')} onClick={closed}>
+        <div>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
+      </div>
+    </Fragment>
+  );
+
+};
+
+export default sideDrawer;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,18 +1,25 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Logo from '../../Logo/Logo';
+import DrawerToggle from '../DrawerToggle/DrawerToggle';
 
 import classes from './Toolbar.module.css';
 
-const toolbar = ({siteTitle}) => (
+const toolbar = ({siteTitle, drawerClicked, sideDrawerOpen}) => (
   <div className={classes.NavContainer}>
+    <DrawerToggle clicked={drawerClicked} sideDrawerOpen={sideDrawerOpen} />
     <div className={classes.Toolbar}>
-      <span>
-        {siteTitle}
-      </span>
-      {/* <nav className={classes.Navigation}> */}
+      <div className={classes.Logo}>
+        <Link to="/">
+          <Logo />
+        </Link>
+      </div>
+        <Link className={classes.Title} to="/">{siteTitle}</Link>
+      <nav className={classes.DesktopOnly}>
         <NavigationItems />
-      {/* </nav> */}
+      </nav>
     </div>
   </div>
 );
