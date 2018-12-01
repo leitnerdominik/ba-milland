33fcd18e9540d61e7567fd607f3d93dd69e5aee6
Dominik Leitner <bigtoasti@gmail.com>
2018-12-02 00:22:32 +0100

    added icons in persondetails

--- src/templates/clubPost.js
+++ src/templates/clubPost.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { graphql } from 'gatsby';
-import Img from 'gatsby-image';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import Layout from '../components/Layout/Layout';
 
@@ -12,13 +12,13 @@ export default ({ data }) => {
   const html = data.markdownRemark.html;
 
 
-  const bannerImg = post.bannerImg !== null ? <img className={classes.BannerImage} src={post.bannerImg.childImageSharp.resize.src} /> : null;
-  const avatarImg = post.avatarImg !== null ? <img className={classes.AvatarImage} src={post.avatarImg.childImageSharp.resize.src} /> : null;
+  const bannerImg = post.bannerImg !== null ? <img className={classes.BannerImage} alt="banner" src={post.bannerImg.childImageSharp.resize.src} /> : null;
+  const avatarImg = post.avatarImg !== null ? <img className={classes.AvatarImage} alt="avatar" src={post.avatarImg.childImageSharp.resize.src} /> : null;
   
-  const name = post.name !== null ? <p><b>Name: </b>{post.name}</p> : null;
-  const email = post.email !== null ? <p><b>Email: </b><a href={`mailto:${post.email}`}>{post.email}</a></p> : null;
-  const tel = post.tel !== null ? <p><b>Tel: </b>{post.tel}</p> : null;
-  const website = post.website !== null ? <p><b>Webseite: </b><a href={`http://${post.website}`}>{post.website}</a></p> : null;
+  const name = post.name !== null ? <p className={classes.Name}>{post.name}</p> : null;
+  const email = post.email !== null ? <p><FontAwesomeIcon icon="envelope" /> <a href={`mailto:${post.email}`}>{post.email}</a></p> : null;
+  const tel = post.tel !== null ? <p><FontAwesomeIcon icon="phone" /> {post.tel}</p> : null;
+  const website = post.website !== null ? <p><FontAwesomeIcon icon="globe-africa" /><a href={`http://${post.website}`}>{post.website}</a></p> : null;
 
   return (
     <Layout>
@@ -32,13 +32,13 @@ export default ({ data }) => {
           <div className={classes.PersonContainer}>
             <div className={classes.AvatarContainer}>
               {avatarImg}
-              <div className={classes.Shadow}></div>
             </div>
             <div className={classes.AvatarDetails}>
               {name}
               {email}
               {tel}
               {website}
+              
             </div>
           </div>
         </div>
