58b6ff84f3ae5ba2cc29713181fc3977cc0bea34
Dominik Leitner <leitner.dominik@hotmail.de>
2018-12-19 21:27:36 +0100

    added a Hero Animation for the root page

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import classes from './Hero.module.css';
+
+const hero = () => (
+  <header className={classes.HeroContainer}>
+    <section>
+      hello
+    </section>
+  </header>
+);
+
+export default hero;
