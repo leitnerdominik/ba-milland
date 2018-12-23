f35966350235d35af156c05e361b98cfdfa72113
Dominik <leitner.dominik@hotmail.de>
2018-12-23 14:09:06 +0100

    added scrolltoContent function, changed style, added text

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
+import Logo from '../../images/bildungsausschuss-logo.png';
+
 import classes from './Hero.module.css';
 
-const hero = () => (
+const hero = ({scrollToContent}) => (
   <header className={classes.HeroContainer}>
-    <section>
-      hello
+    <section className={classes.ContentContainer}>
+      <div className={classes.LogoContainer}>
+        <img src={Logo} className={classes.Logo}/><span>Bildungsausschuss</span>
+      </div>
+      <div>
+        <span>Milland</span>
+      </div>
+      <button onClick={scrollToContent}>DOWN</button>
     </section>
   </header>
 );
