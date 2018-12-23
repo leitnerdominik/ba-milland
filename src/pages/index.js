3c01cdf3621240f63ae460f5f8401559c43bb489
Dominik <leitner.dominik@hotmail.de>
2018-12-23 14:11:39 +0100

    added ref to scroll to content, added centered text

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { Component } from "react";
 
-import Layout from '../components/Layout/Layout';
-import Hero from '../components/Hero/Hero';
-import EventsPreview from '../components/EventsPreview/EventsPreview';
+import Layout from "../components/Layout/Layout";
+import Hero from "../components/Hero/Hero";
+import EventsPreview from "../components/EventsPreview/EventsPreview";
 
-const IndexPage = () => {
-   
-  return (
-    <Layout>
-      <Hero />
-      <EventsPreview />
-    </Layout>
-  );
+import classes from "./index.module.css";
+
+class IndexPage extends Component {
+
+  contentStart = React.createRef();
+
+  scrollToContent = e => {
+    this.contentStart.current.scrollIntoView({block: "start", behavior: "smooth"});
+  };
+
+  render() {
+    return (
+      <Layout>
+        <Hero scrollToContent={this.scrollToContent}/>
+        <div ref={this.contentStart} className={classes.IndexContainer}>
+          <EventsPreview />
+        </div>
+      </Layout>
+    );
+  }
 }
 
 export default IndexPage;
