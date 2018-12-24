d99fb8adeacc020c84d3a1df2627fa3565f745a0
Dominik Leitner <bigtoasti@gmail.com>
2018-12-24 12:33:11 +0100

    showing logo with gatsby-image

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,19 +1,21 @@
 import React from 'react';
+import Img from 'gatsby-image';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-import Logo from '../../images/bildungsausschuss-logo.png';
+// import Logo from '../../images/bildungsausschuss-logo-big.png';
 
 import classes from './Hero.module.css';
 
-const hero = ({scrollToContent}) => (
+const hero = ({scrollToContent, logo}) => (
   <header className={classes.HeroContainer}>
     <section className={classes.ContentContainer}>
       <div className={classes.LogoContainer}>
-        <img src={Logo} className={classes.Logo}/><span>Bildungsausschuss</span>
+        <Img fluid={logo} className={classes.Logo}/><span>Bildungsausschuss</span>
       </div>
       <div>
         <span>Milland</span>
       </div>
-      <button onClick={scrollToContent}>DOWN</button>
+      <button className={classes.ButtonDown} onClick={scrollToContent}><FontAwesomeIcon icon="angle-down"/></button>
     </section>
   </header>
 );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,27 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import { graphql } from 'gatsby';
 
-import Layout from "../components/Layout/Layout";
-import Hero from "../components/Hero/Hero";
-import EventsPreview from "../components/EventsPreview/EventsPreview";
+import Layout from '../components/Layout/Layout';
+import Hero from '../components/Hero/Hero';
+import EventsPreview from '../components/EventsPreview/EventsPreview';
 
-import classes from "./index.module.css";
+import classes from './index.module.css';
 
 class IndexPage extends Component {
-
   contentStart = React.createRef();
 
   scrollToContent = e => {
-    this.contentStart.current.scrollIntoView({block: "start", behavior: "smooth"});
+    this.contentStart.current.scrollIntoView({
+      block: 'start',
+      behavior: 'smooth',
+    });
   };
 
   render() {
+    const logo = this.props.data.file.childImageSharp.fluid;
     return (
       <Layout>
-        <Hero scrollToContent={this.scrollToContent}/>
+        <Hero scrollToContent={this.scrollToContent} logo={logo} />
         <div ref={this.contentStart} className={classes.IndexContainer}>
           <EventsPreview />
         </div>
@@ -27,3 +31,15 @@ class IndexPage extends Component {
 }
 
 export default IndexPage;
+
+export const query = graphql`
+query {
+  file(relativePath: { eq: "images/bildungsausschuss-logo-big.png" }) {
+    childImageSharp {
+      fluid(maxHeight: 250, maxWidth: 250) {
+        ...GatsbyImageSharpFluid
+      }
+    }
+  }
+}
+`;
