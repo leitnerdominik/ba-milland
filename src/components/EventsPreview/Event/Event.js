277aabfd9df1e9cf8b175469e8196a9dc6794a74
Dominik Leitner <bigtoasti@gmail.com>
2019-01-29 18:06:24 +0100

    added a little badge on the corner

--- src/components/EventsPreview/Event/Event.js
+++ src/components/EventsPreview/Event/Event.js
@@ -3,33 +3,50 @@ import { Link } from 'gatsby';
 
 import classes from './Event.module.css';
 
-const event = ({ title, date_from, date_until, time_from, time_until, content, path }) => {
+const event = props => {
+  const {
+    title,
+    date_from,
+    date_until,
+    time_from,
+    time_until,
+    club,
+    badgetColor,
+    content,
+    path,
+  } = props;
   const renderContent = content ? <p>{content}</p> : null;
 
   let renderTime = null;
-  console.log(time_until);
-  if(time_from && time_until) {
-    renderTime = <p className={classes.TimeContainer}><b>Zeit: </b> {time_from} - {time_until}</p>
-  } else if(time_from) {
-    renderTime = <p className={classes.TimeContainer}><b>Zeit: </b> {time_from}</p>
+  if (time_from && time_until) {
+    renderTime = (
+      <p className={classes.TimeContainer}>
+        <b>Zeit: </b> {time_from} - {time_until}
+      </p>
+    );
+  } else if (time_from) {
+    renderTime = (
+      <p className={classes.TimeContainer}>
+        <b>Zeit: </b> {time_from}
+      </p>
+    );
   }
 
   let renderDate = date_from;
-  if(date_from && date_until) {
+  if (date_from && date_until.trim().length > 0) {
     renderDate = `${date_from} - ${date_until}`;
   }
 
   return (
     <div className={classes.EventContainer}>
+      <h5 className={classes.EventDate}>{renderDate}</h5>
       <Link className={classes.EventLink} to={path}>
         <h3>{title}</h3>
       </Link>
-      <h5 className={classes.EventDate}>
-        {renderDate}
-      </h5>
-      <br />
+      <span style={{
+        background: `${badgetColor}`
+      }} className={classes.Club}>{club}</span>
       {renderTime}
-      <br />
       {renderContent}
     </div>
   );
--- src/components/EventsPreview/EventsPreview.js
+++ src/components/EventsPreview/EventsPreview.js
@@ -3,60 +3,79 @@ import { StaticQuery, graphql } from 'gatsby';
 import moment from 'moment';
 
 import Event from './Event/Event';
+import mapClubToColor from '../../utils/badget-color';
 
 import classes from './EventsPreview.module.css';
 
 const events = () => (
   <StaticQuery
-    query={graphql`query {
-      allMarkdownRemark(sort: { fields: [frontmatter___date_from], order: ASC }) {
-        edges {
-          node {
-            id
-            frontmatter {
-              title
-              date_from
-              date_until
-              time_from
-              time_until
+    query={graphql`
+      query {
+        allMarkdownRemark(
+          sort: { fields: [frontmatter___date_from], order: ASC }
+        ) {
+          edges {
+            node {
+              id
+              frontmatter {
+                title
+                date_from
+                date_until
+                time_from
+                time_until
+                club
+              }
+              fields {
+                slug
+              }
+              excerpt
             }
-            fields {
-              slug
-            }
-            excerpt
           }
         }
       }
-    }
-  `}
+    `}
     render={data => {
-      const postsOlderThanToday = data.allMarkdownRemark.edges.filter(({node}) => {
-        const today = moment().startOf('day');
-        const postDate = (moment(node.frontmatter.date_from, "DD.MM.YYYY"));
-        return today.isSameOrBefore(postDate);
+      const postsOlderThanToday = data.allMarkdownRemark.edges.filter(
+        ({ node }) => {
+          const today = moment().startOf('day');
+          const postDate = moment(node.frontmatter.date_from, 'DD.MM.YYYY');
+          return today.isSameOrBefore(postDate);
+        }
+      );
+
+      const events = postsOlderThanToday.map(({ node }) => {
+        const badget = mapClubToColor(node.frontmatter.club.toString().toLowerCase());
+        console.log(badget, node.frontmatter.club);
+        return (
+          <Event
+            key={node.id}
+            title={node.frontmatter.title}
+            date_from={node.frontmatter.date_from}
+            date_until={node.frontmatter.date_until}
+            time_from={node.frontmatter.time_from}
+            time_until={node.frontmatter.time_until}
+            club={node.frontmatter.club}
+            badgetColor={badget}
+            path={node.fields.slug}
+            content={node.excerpt}
+          />
+        );
       });
 
-      const events = postsOlderThanToday.map(({ node }) => (
-        <Event 
-          key={node.id}
-          title={node.frontmatter.title}
-          date_from={node.frontmatter.date_from}
-          date_until={node.frontmatter.date_until}
-          time_from={node.frontmatter.time_from}
-          time_until={node.frontmatter.time_until}
-          path={node.fields.slug}
-          content={node.excerpt}
-          />
-      ));
-
-      const content = events.length > 0 ? events : <p className={classes.InfoText}>Keine Einträge gefunden!</p>
+      const content =
+        events.length > 0 ? (
+          events
+        ) : (
+          <p className={classes.InfoText}>Keine Einträge gefunden!</p>
+        );
       return (
-      <>
-        <h2 className={classes.Title}>Bevorstehende Veranstaltungen</h2>
-        {content}
-      </>
-      )}} 
+        <>
+          <h2 className={classes.Title}>Bevorstehende Veranstaltungen</h2>
+          <div className={classes.EventsContainer}>{content}</div>
+        </>
+      );
+    }}
   />
-)
+);
 
 export default events;
