fc750e8dfe062531f7566bae6107c30de7aebaec
Dominik Leitner <bigtoasti@gmail.com>
2019-06-20 21:18:06 +0200

    remove nav-item "veranstaltungen"

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -8,7 +8,7 @@ import classes from './NavigationItems.module.css';
 const navigationItems = () => (
   <ul className={classes.NavigationItems}>
     <NavigationItem path="/">Milland</NavigationItem>
-    <NavigationItem path="/veranstaltungen">Veranstaltungen</NavigationItem>
+    {/* <NavigationItem path="/veranstaltungen">Veranstaltungen</NavigationItem> */}
     <NavigationItem path="/previewclubs">Vereine</NavigationItem>
     <NavigationItem path="/geschichte">Geschichte</NavigationItem>
     <NavigationItem path="http://www.pfarrei-milland.it/Aktuelles%20PDF/PDFdruck.pdf" extern>Pfarrblatt</NavigationItem>
