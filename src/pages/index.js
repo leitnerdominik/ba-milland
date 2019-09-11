7b11e6cc4e0b26bd8b7cd501f7b7811a47d57b87
Dominik Leitner <leitner.dominik@hotmail.de>
2019-09-11 18:41:39 +0200

    removed news

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Layout from '../components/Layout/Layout';
-import Hero from '../components/Hero/Hero';
-import AboutUs from '../components/AboutUs/AboutUs';
-import EventsPreview from '../components/EventsPreview/EventsPreview';
+import Layout from "../components/Layout/Layout";
+import Hero from "../components/Hero/Hero";
+import AboutUs from "../components/AboutUs/AboutUs";
+import EventsPreview from "../components/EventsPreview/EventsPreview";
 
-import classes from '../styles/pages/index/index.module.css';
+import classes from "../styles/pages/index/index.module.css";
 
 class IndexPage extends Component {
   contentStart = React.createRef();
 
   scrollToContent = () => {
     this.contentStart.current.scrollIntoView({
-      block: 'start',
-      behavior: 'smooth',
+      block: "start",
+      behavior: "smooth"
     });
   };
 
