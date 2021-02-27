616132cdd434e658585a36bbd429347c1e4cc7f8
Dominik <leitner.dominik@hotmail.de>
2021-02-27 19:05:30 +0100

    add chronik

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import NavigationItem from './NavigationItem/NavigationItem';
 import mize from '../../../assets/mize.pdf';
+import chronik2020 from '../../../assets/Chronik2020.pdf';
 
 import classes from './NavigationItems.module.css';
 
@@ -13,6 +14,7 @@ const navigationItems = () => (
     <NavigationItem path="/geschichte">Geschichte</NavigationItem>
     <NavigationItem path="http://www.pfarrei-milland.it/Aktuelles%20PDF/PDFdruck.pdf" extern>Pfarrblatt</NavigationItem>
     <NavigationItem path={mize} extern>MiZe</NavigationItem>
+    <NavigationItem path={chronik2020} extern>Chronik</NavigationItem>
   </ul>
 );
 
